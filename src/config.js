cf54a4538edb6a287e3aaf332e7fb3bfd57afde2
Steven Dao <iamstevendao@gmail.com>
2018-08-12 12:39:45 +1000

    Add eslint airbnb

--- src/config.js
+++ src/config.js
@@ -1,5 +1,13 @@
-export default {
-  gameWidth: 760,
-  gameHeight: 400,
-  localStorageName: 'phaseres6webpack'
+
+function getWindowSize() {
+  // get the smaller dimension
+  const w = window.innerWidth;
+  const h = window.innerHeight;
+  return w > h ? h - 150 : w - 150;
 }
+
+export default {
+  gameWidth: getWindowSize(),
+  gameHeight: getWindowSize(),
+  localStorageName: 'monkey-maths',
+};
