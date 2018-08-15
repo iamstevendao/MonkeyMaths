b562d9398f195b5cbdf84ce8e5f1a41f4be33315
Steven Dao <iamstevendao@gmail.com>
2018-08-15 23:36:17 +1000

    Random operation in question

--- src/utils/Question.ts
+++ src/utils/Question.ts
@@ -1,23 +1,63 @@
 // Question
 
+const operators = [{
+  sign: '+',
+  method: (a, b) => (a + b),
+}, {
+  sign: '-',
+  method: (a, b) => (a - b),
+}, {
+  sign: '/',
+  method: (a, b) => Math.floor(a / b),
+}, {
+  sign: '*',
+  method: (a, b) => (a * b),
+}];
+
+function random(to, from = 0): number {
+  return Math.floor(Math.random() * to + from);
+}
+
 export class Question {
   private number1: number;
   private number2: number;
-  private operator: string;
+  private operator: any;
   private result: string;
 
-  constructor({ start = 0, end = 0, operator = '+' } = {}) {
-    this.operator = operator || '+';
-    this.number1 = start || 10;
-    this.number2 = end || 2;
-    this.result = (this.number1 + this.number2).toString();
+  constructor() {
+    this.operator = operators[random(operators.length)];
+    this.generateNumbers();
+    this.result = this.operator.method(this.number1, this.number2);
+  }
+
+  private generateNumbers() {
+    switch (this.operator.sign) {
+      case '+':
+        this.number1 = random(10);
+        this.number2 = random(10);
+        break;
+      case '-':
+        this.number2 = random(10);
+        this.number1 = random(20, this.number2);
+        break;
+      case '*':
+        this.number1 = random(5);
+        this.number2 = random(5);
+        break;
+      case '/':
+        this.number2 = random(5, 1);
+        this.number1 = this.number2 * random(5);
+        break;
+      default:
+        break;
+    }
   }
 
   public getText(): string {
-    return `${this.number1} ${this.operator} ${this.number2} =`;
+    return `${this.number1} ${this.operator.sign} ${this.number2} =`;
   }
 
   public getResult(): string {
-    return this.result;
+    return this.result.toString();
   }
 }
